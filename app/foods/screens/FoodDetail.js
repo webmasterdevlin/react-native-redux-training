88c15dc46bbe5a41102830485f4c5690e0a412ad
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-09-22 15:43:33 +0200

    fixed foodDetail getParams error

--- app/foods/screens/FoodDetail.js
+++ app/foods/screens/FoodDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Text} from 'native-base';
 
 const FoodDetail = props => (
-  <Text style={{fontSize: 40}}>{props.navigation.getParam('what').name}</Text>
+  <Text style={{fontSize: 40}}>{props.navigation.getParam('id').name}</Text>
 );
 
 export default FoodDetail;
